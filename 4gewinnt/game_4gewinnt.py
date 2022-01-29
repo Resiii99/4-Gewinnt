67e7ce24c1f4b39ccd22b17e0f814ff7b83aa051
Juliane Brantner <juliane.brantner@gmx.at>
2022-01-29 14:18:46 +0100

    Spalte angeben
    Mit einem Index-Parameter soll die Spalte angegeben werden können.

--- 4gewinnt/game_4gewinnt.py
+++ 4gewinnt/game_4gewinnt.py
@@ -3,3 +3,7 @@ class VierGewinnt:
         self.hoehe = hoehe
         self.breite = breite
         self.spielbrett = [[' ' for x in range(breite)] for i in range(hoehe)]
+
+    def get_spalte(self, index):
+        return [i[index] for i in self.spielbrett]
+    
