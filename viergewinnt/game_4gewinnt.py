1ba76b64906b658d1484301e9f75dcf66d5315e7
Theresa Lohr <s50833@edu.campus02.at>
2022-02-16 21:57:44 +0100

    Unentschieden und "maximale Spaltenanzahl erreicht" implementiert

--- viergewinnt/game_4gewinnt.py
+++ viergewinnt/game_4gewinnt.py
@@ -13,6 +13,7 @@ class VierGewinnt:
         self.hoehe = hoehe
         self.breite = breite
         self.spielbrett = [[' ' for x in range(breite)] for i in range(hoehe)]
+        self.__gespielte_Steine = 0
 
     def get_spalte(self, index: int):
         """
@@ -85,15 +86,8 @@ class VierGewinnt:
         while self.spielbrett[i][spl] != ' ':
             i -= 1
         self.spielbrett[i][spl] = team
+        self.__gespielte_Steine += 1
         return self.spielbrett
-
-    def alle_Felder_bespielt(self):
-        """
-        Überprüft ob auf dem Spielbrett noch Felder frei sind.
-        Falls dies nicht der Fall ist soll das Spiel beeendet werden.
-        """
-        maxspielsteine = self.breite*self.hoehe
-
 
     def check_gewonnen(self):
         """
@@ -133,15 +127,24 @@ class VierGewinnt:
 
         return None
 
+    def check_unentschieden(self):
+        """
+        Wenn das Spielfeld voll ist, soll das Spiel mit einem Unentschieden beendet werden.
+        """
+        maxspielsteine = self.breite*self.hoehe
+
+        if self.__gespielte_Steine == maxspielsteine:
+            return True
+        else:
+            return None
+
     def spiel_starten(self):
         """
         Startet ein Spiel. Das Spielfeld wird dargestellt, die Spieler werden nacheinander zum Input aufgefordert und
         ihre Spielzüge werden durchgeführt. Nach jeder Durchführung wird auf Gewinn überprüft. Bei Gewinn eines Spielers
         wird abgebrochen.
         """
-
         print("Willkommen bei 4 Gewinnt - vorab musst du festlegen, ob du gegen den Computer oder gegen einen Freund spielst:")
-
         spieleranzahl = int(input(f'Wähle 1 für ein Spiel gegen den Computer, oder 2 für ein Spiel gegen deinen Freund: '))
 
         if spieleranzahl == 2:
@@ -151,11 +154,16 @@ class VierGewinnt:
                     print(i)
                 if self.check_gewonnen() is not None:
                     break
+                if self.check_unentschieden() is True:
+                    print("Unentschieden - Das spiel ist Vorbei!")
+                    return None
 
                 spl = int(input(f'Spieler 1 – wähle eine Spalte von 1 bis {self.breite} : ')) - 1
 
-                if spl > -1 and spl < self.breite:
+                if spl > -1 and spl < self.breite and self.spielbrett[0][spl] == ' ':
                     self.spielzug_machen('X', spl)
+                elif spl > -1 and spl < self.breite and self.spielbrett[0][spl] != ' ':
+                    print(f'Diese Spalte ist voll. Wähle in der nächsten Runde ein leeres Feld.')
                 else:
                     print(f'Bitte geben Sie  in der nächsten Runde eine Zahl zwischen 1 und {self.breite} ein!')
 
@@ -163,11 +171,16 @@ class VierGewinnt:
                     print(i)
                 if self.check_gewonnen() is not None:
                     break
+                if self.check_unentschieden() is True:
+                    print("Unentschieden - Das spiel ist Vorbei!")
+                    return None
 
                 spl = int(input(f'Spieler 2 – bitte Spalte von 1 bis {self.breite} auswählen: ')) - 1
 
-                if spl > -1 and spl < self.breite:
+                if spl > -1 and spl < self.breite and self.spielbrett[0][spl] == ' ':
                     self.spielzug_machen('O', spl)
+                elif spl > -1 and spl < self.breite and self.spielbrett[0][spl] != ' ':
+                    print(f'Diese Spalte ist voll. Wähle in der nächsten Runde ein leeres Feld.')
                 else:
                     print(f'Bitte geben Sie in der nächsten Runde eine Zahl zwischen 1 und {self.breite} ein!')
 
@@ -181,11 +194,16 @@ class VierGewinnt:
                     print(i)
                 if self.check_gewonnen() is not None:
                     break
+                if self.check_unentschieden() is True:
+                    print("Unentschieden - Das spiel ist Vorbei!")
+                    return None
 
                 spl = int(input(f'Wähle eine Spalte von 1 bis {self.breite} : ')) - 1
 
-                if spl > -1 and spl < self.breite:
-                    self.spielzug_machen('X', spl)
+                if spl > -1 and spl < self.breite and self.spielbrett[0][spl] == ' ':
+                    self.spielzug_machen('O', spl)
+                elif spl > -1 and spl < self.breite and self.spielbrett[0][spl] != ' ':
+                    print(f'Diese Spalte ist voll. Wähle in der nächsten Runde ein leeres Feld.')
                 else:
                     print(f'Spielzug verschenkt! Gib in der nächsten Runde eine Zahl zwischen 1 und {self.breite} ein!')
 
@@ -193,6 +211,9 @@ class VierGewinnt:
                     print(i)
                 if self.check_gewonnen() is not None:
                     break
+                if self.check_unentschieden() is True:
+                    print("Unentschieden - Das spiel ist Vorbei!")
+                    return None
 
                 computer = randint(0, self.breite-1)
                 print("Der Computer wählt die Spalte ", computer + 1)
@@ -202,11 +223,13 @@ class VierGewinnt:
                 print(f'Gratuliere {self.check_gewonnen()}, du hast den Computer geschlagen!')
             else:
                 print("Schade, diese Runde geht an den Computer - Versuchs doch noch mal :-) ")
-
+            if self.check_unentschieden() is True:
+                print("Unentschieden - Das spiel ist Vorbei!")
+                return None
         else:
             print("Starte das Spiel erneut und wähle eine gültige Spieleranzahl (1 oder 2)")
 
 
 if __name__ == '__main__':
-    meinspiel = VierGewinnt(7,7)
+    meinspiel = VierGewinnt()
     meinspiel.spiel_starten()
